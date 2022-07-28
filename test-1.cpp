71363d3e9423aa61a0b51a2fb039d164dda8fbdc
Suleyman TURKMEN <sturkmen@hotmail.com>
2022-07-28 10:09:21 +0300

    test-1.cpp

--- test-1.cpp
+++ test-1.cpp
@@ -1,93 +1,64 @@
-#include "opencv2/core.hpp"
-#include "opencv2/imgcodecs.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 #include <iostream>
-
-using namespace std;
 using namespace cv;
+using namespace std;
 
-int main(int argc, char** argv)
+void createAlphaMat(Mat& mat)
 {
-    if (argc < 2)
-        return 0;
-
-    const char* names[] = { "IMREAD_SUCCESS", "IMREAD_FILE_NOT_OPENED",
-        "IMREAD_UNKNOWN_FILE_TYPE","IMREAD_READ_HEADER_ERROR", "IMREAD_READ_DATA_ERROR" };
-
-    String filename0 = argv[1];
-    imquery iminfo1(filename0);
-
-    cout << "file name : " << filename0 << endl;
-    cout << "page_count : " << iminfo1.page_count() << endl;
-    cout << "imquery result code : " << names[iminfo1.result_code()] << endl << endl;
-
-    for (int i = 0; i < iminfo1.page_count(); i++)
-    {
-        cout << "page index : " << i << endl;
-        cout << "type : " << iminfo1.type(i) << endl;
-        cout << "width : " << iminfo1.width(i) << endl;
-        cout << "height : " << iminfo1.height(i) << endl;
-        cout << "scalable : " << iminfo1.scalable(i) << endl << endl;
+    CV_Assert(mat.channels() == 4);
+    for (int i = 0; i < mat.rows; ++i) {
+        for (int j = 0; j < mat.cols; ++j) {
+            Vec4b& bgra = mat.at<Vec4b>(i, j);
+            bgra[0] = UCHAR_MAX; // Blue
+            bgra[1] = saturate_cast<uchar>((float(mat.cols - j)) / ((float)mat.cols) * UCHAR_MAX); // Green
+            bgra[2] = saturate_cast<uchar>((float(mat.rows - i)) / ((float)mat.rows) * UCHAR_MAX); // Red
+            bgra[3] = saturate_cast<uchar>(0.5 * (bgra[1] + bgra[2])); // Alpha
+        }
     }
+}
 
-    Mat img;
-    cout << "imread result code: " << names[imread(filename0, img, IMREAD_REDUCED_COLOR_2, 2)] << endl << endl;
+int pngWritingParamsTest()
+{
+    // Create mat with alpha channel
+    Mat mat(480, 640, CV_8UC4);
+    createAlphaMat(mat);
 
-    if (argc < 3)
-        return 0;
+    vector<int> compression_params;
+    compression_params.push_back(IMWRITE_PNG_COMPRESSION);
+    compression_params.push_back(0);
+    compression_params.push_back(IMWRITE_PNG_STRATEGY);
+    compression_params.push_back(IMWRITE_PNG_STRATEGY_DEFAULT);
+    for (int i = 0; i < 10; i++)
+        for (int j = 0; j < 5; j++)
+        {
+            compression_params[1] = i;
+            compression_params[3] = j;
+            imwrite(format("PNG_STRATEGY_%d_PNG_COMPRESSION_%d.png", j, i), mat, compression_params);
+        }
+    imwrite("PNG_SAVED_DEFAULT.png", mat);
+    return 0;
+}
 
-    String filename1 = argv[2];
-    imquery iminfo2(filename1);
+int main()
+{
+    pngWritingParamsTest();
 
-    cout << "file name : " << filename1 << endl;
-    cout << "page_count : " << iminfo2.page_count() << endl;
-    cout << "imquery result code : " << names[iminfo2.result_code()] << endl << endl;
+    vector<String> filenames;
+    String folder = "C:/projects/opencv-image-reading/*.png";
+    glob(folder, filenames);
 
-    for (int i = 0; i < iminfo2.page_count(); i++)
+    for (size_t i = 0; i < filenames.size(); i++)
     {
-        cout << "page index : " << i << endl;
-        cout << "type : " << iminfo2.type(i) << endl;
-        cout << "width : " << iminfo2.width(i) << endl;
-        cout << "height : " << iminfo2.height(i) << endl;
-        cout << "scalable : " << iminfo2.scalable(i) << endl << endl;
+        Mat src = imread(filenames[i], IMREAD_UNCHANGED);
+
+        if (src.empty())
+            std::cout << filenames[i] << "--------- Could not read the image" << std::endl;
+        else
+        {
+            std::cout << filenames[i] << std::endl;
+        }
+
     }
-
-    Mat roi = img(Rect(30, 100, iminfo2.width() / 2, iminfo2.height() / 2));
-    cout << "imread result code: " << imread(filename1, roi, IMREAD_REDUCED_COLOR_2) << endl;
-
-    imwrite("multipage-2.jpg", img);
-
-    vector<Mat> images;
-    images.push_back(img);
-    images.push_back(roi);
-    Mat gray;
-    cvtColor(roi, gray, COLOR_BGR2GRAY);
-    images.push_back(gray);
-    Mat grayf;
-    gray.convertTo(grayf, CV_32F);
-    images.push_back(grayf);
-    gray.convertTo(grayf, CV_32S);
-    images.push_back(grayf);
-    imwritemulti("multipage-test.tif", images);
-
-    imquery iminfo3("multipage-test.tif");
-
-    cout << "\n+++++++ here we are reading the file we wrote earlier +++++++++++\n\n";
-    cout << "file name : multipage-test.tif" << endl;
-    cout << "page_count : " << iminfo3.page_count() << endl;
-    cout << "imquery result code : " << names[iminfo3.result_code()] << endl << endl;
-
-    for (int i = 0; i < iminfo3.page_count(); i++)
-    {
-        cout << "page index : " << i << endl;
-        cout << "type : " << iminfo3.type(i) << endl;
-        cout << "width : " << iminfo3.width(i) << endl;
-        cout << "height : " << iminfo3.height(i) << endl;
-        cout << "scalable : " << iminfo3.scalable(i) << endl << endl;
-    }
-
-    imshow("multipage-2.jpg", img);
-    waitKey(1000);
     return 0;
 }
