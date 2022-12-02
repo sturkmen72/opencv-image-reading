27d5b8bf3190f5da7b70a5e9372866f4acca58f5
Suleyman TURKMEN <sturkmen@hotmail.com>
2022-12-02 06:00:26 +0300

    Update test-1.cpp

--- test-1.cpp
+++ test-1.cpp
@@ -9,85 +9,30 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
-    if (argc < 2)
-        return 0;
+    Mat img0 = imread(samples::findFile("C:\\projects\\opencv-image-reading\\source1.png"));
+    TickMeter tm;
+    Mat inImage, SplicingImage;
+    cv::resize(img0, inImage, Size(936, 936)); //Set the size of the input image
+    tm.reset();
+    tm.start();
+    hconcat(inImage, inImage, SplicingImage);
+    vconcat(SplicingImage, SplicingImage, SplicingImage);
+    Mat mask = 255 * Mat::ones(inImage.rows, inImage.cols, inImage.depth());
+    Point centerPt = Point(SplicingImage.cols / 2, SplicingImage.rows / 2);
+    seamlessClone(inImage, SplicingImage, mask, centerPt, SplicingImage, NORMAL_CLONE);
+    tm.stop();
+    imwrite("cloned1.png", SplicingImage);
+    cout << tm << endl;
 
-    const char* names[] = { "IMREAD_SUCCESS", "IMREAD_FILE_NOT_OPENED",
-        "IMREAD_UNKNOWN_FILE_TYPE","IMREAD_READ_HEADER_ERROR", "IMREAD_READ_DATA_ERROR" };
-
-    String filename0 = argv[1];
-    imquery iminfo1(filename0);
-
-    cout << "file name : " << filename0 << endl;
-    cout << "page_count : " << iminfo1.page_count() << endl;
-    cout << "imquery result code : " << names[iminfo1.result_code()] << endl << endl;
-
-    for (int i = 0; i < iminfo1.page_count(); i++)
-    {
-        cout << "page index : " << i << endl;
-        cout << "type : " << iminfo1.type(i) << endl;
-        cout << "width : " << iminfo1.width(i) << endl;
-        cout << "height : " << iminfo1.height(i) << endl;
-        cout << "scalable : " << iminfo1.scalable(i) << endl << endl;
-    }
-
-    Mat img;
-    cout << "imread result code: " << names[imread(filename0, img, IMREAD_REDUCED_COLOR_2, 2)] << endl << endl;
-
-    if (argc < 3)
-        return 0;
-
-    String filename1 = argv[2];
-    imquery iminfo2(filename1);
-
-    cout << "file name : " << filename1 << endl;
-    cout << "page_count : " << iminfo2.page_count() << endl;
-    cout << "imquery result code : " << names[iminfo2.result_code()] << endl << endl;
-
-    for (int i = 0; i < iminfo2.page_count(); i++)
-    {
-        cout << "page index : " << i << endl;
-        cout << "type : " << iminfo2.type(i) << endl;
-        cout << "width : " << iminfo2.width(i) << endl;
-        cout << "height : " << iminfo2.height(i) << endl;
-        cout << "scalable : " << iminfo2.scalable(i) << endl << endl;
-    }
-
-    Mat roi = img(Rect(30, 100, iminfo2.width() / 2, iminfo2.height() / 2));
-    cout << "imread result code: " << imread(filename1, roi, IMREAD_REDUCED_COLOR_2) << endl;
-
-    imwrite("multipage-2.jpg", img);
-
-    vector<Mat> images;
-    images.push_back(img);
-    images.push_back(roi);
-    Mat gray;
-    cvtColor(roi, gray, COLOR_BGR2GRAY);
-    images.push_back(gray);
-    Mat grayf;
-    gray.convertTo(grayf, CV_32F);
-    images.push_back(grayf);
-    gray.convertTo(grayf, CV_32S);
-    images.push_back(grayf);
-    imwritemulti("multipage-test.tif", images);
-
-    imquery iminfo3("multipage-test.tif");
-
-    cout << "\n+++++++ here we are reading the file we wrote earlier +++++++++++\n\n";
-    cout << "file name : multipage-test.tif" << endl;
-    cout << "page_count : " << iminfo3.page_count() << endl;
-    cout << "imquery result code : " << names[iminfo3.result_code()] << endl << endl;
-
-    for (int i = 0; i < iminfo3.page_count(); i++)
-    {
-        cout << "page index : " << i << endl;
-        cout << "type : " << iminfo3.type(i) << endl;
-        cout << "width : " << iminfo3.width(i) << endl;
-        cout << "height : " << iminfo3.height(i) << endl;
-        cout << "scalable : " << iminfo3.scalable(i) << endl << endl;
-    }
-
-    imshow("multipage-2.jpg", img);
-    waitKey(1000);
+    cv::resize(img0, inImage, Size(937, 937)); //Set the size of the input image
+    tm.reset();
+    tm.start();
+    hconcat(inImage, inImage, SplicingImage);
+    vconcat(SplicingImage, SplicingImage, SplicingImage);
+    Mat mask2 = 255 * Mat::ones(inImage.rows, inImage.cols, inImage.depth());
+    seamlessClone(inImage, SplicingImage, mask2, centerPt, SplicingImage, NORMAL_CLONE);
+    tm.stop();
+    cout << tm << endl;
+    imwrite("cloned2.png", SplicingImage);
     return 0;
 }
